f8e5ed3aff9189ef08c939aba0e4054b25648231
fzdocente <fzdocente@gmail.com>
2023-02-10 08:40:41 -0500

    se crea ejemplo1

---
+++ main.js
@@ -0,0 +1,10 @@
+var jugador="James Rodriguez"
+
+if(true){
+    var jugador="Lucho diaz"
+}
+
+const VALOR_IVA=0.19
+
+console.log("hola "+jugador+" como estas")
+console.log(`hola ${jugador} como estas`)
